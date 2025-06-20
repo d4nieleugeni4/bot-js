3bc5a950f7229f5e99c2342d158dd04b6b148600
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-06-20 19:56:49 -0300

    Delete comandos/ping.js

--- comandos/ping.js
+++
@@ -1,7 +0,0 @@
-export default {
-    name: 'ping',
-    description: 'Responde com "Pong!"',
-    async execute(sock, message) {
-        await sock.sendMessage(message.key.remoteJid, { text: 'Pong!' });
-    }
-};
